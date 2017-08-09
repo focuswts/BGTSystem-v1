27bbc400358ebcdd63fc73317b8f61c48ca327b5
focuswts <felipebg2008@hotmail.com>
2017-08-09 18:32:16 -0300

    V3.0

--- src/Administrator/MenuAdmin.java
+++ src/Administrator/MenuAdmin.java
@@ -13,6 +13,7 @@ import InternalGUI.CAD_Clientes;
 import InternalGUI.CAD_Funcionarios;
 import InternalGUI.Login;
 import InternalGUI.CAD_Usuarios;
+import Relatorio.relatorioGUI;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.text.SimpleDateFormat;
@@ -70,6 +71,8 @@ public class MenuAdmin extends javax.swing.JFrame {
         jMenu3 = new javax.swing.JMenu();
         jMenuItem6 = new javax.swing.JMenuItem();
         jMenuItem9 = new javax.swing.JMenuItem();
+        jMenu6 = new javax.swing.JMenu();
+        jMenuItem10 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Bagatim System V1");
@@ -219,6 +222,18 @@ public class MenuAdmin extends javax.swing.JFrame {
 
         jMenuBar1.add(jMenu3);
 
+        jMenu6.setText("Relatórios");
+
+        jMenuItem10.setText("Gerar Relatório");
+        jMenuItem10.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jMenuItem10ActionPerformed(evt);
+            }
+        });
+        jMenu6.add(jMenuItem10);
+
+        jMenuBar1.add(jMenu6);
+
         setJMenuBar(jMenuBar1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -305,6 +320,12 @@ public class MenuAdmin extends javax.swing.JFrame {
         viewVendas.setVisible(true);
     }//GEN-LAST:event_jMenuItem9ActionPerformed
 
+    private void jMenuItem10ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem10ActionPerformed
+relatorioGUI viewRelatorio = new relatorioGUI();
+dp_DesktopPane.add(viewRelatorio);
+viewRelatorio.setVisible(true);
+    }//GEN-LAST:event_jMenuItem10ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -376,8 +397,10 @@ public class MenuAdmin extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu3;
     private javax.swing.JMenu jMenu4;
     private javax.swing.JMenu jMenu5;
+    private javax.swing.JMenu jMenu6;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
+    private javax.swing.JMenuItem jMenuItem10;
     private javax.swing.JMenuItem jMenuItem2;
     private javax.swing.JMenuItem jMenuItem3;
     private javax.swing.JMenuItem jMenuItem4;
